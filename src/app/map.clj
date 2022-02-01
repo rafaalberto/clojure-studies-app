4586c597e712e7a7f23bc93132457df6e8a1bcc1
Rafael Alberto <rafael.alberto1703@gmail.com>
2022-02-01 20:50:59 -0300

    add filter

--- src/app/map.clj
+++ src/app/map.clj
@@ -1,6 +1,15 @@
 (ns app.map)
 
-(def transactions {:value 30.0 :type "Expense"})
+(def transactions
+  [{:value 30.0, :type "Expense", :date "2022-02-01"}
+   {:value 300.0, :type "Savings", :date "2022-01-19"}
+   {:value 50.0, :type "Expense", :date "2022-01-10"}])
 
-;(println (get transactions :type)) ;another option
-(println (:type transactions))
+(defn report [transactions]
+  (select-keys transactions [:value :date]))
+
+(defn expense? [transactions]
+  (= (:type transactions) "Expense"))
+
+(println "Report: " (map report transactions))
+(println "Expenses: " (filter expense? transactions))
