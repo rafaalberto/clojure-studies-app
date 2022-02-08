875af097eb21d9e96b4bf25e6999dd4ae3550650
Rafael Alberto <rafael.alberto1703@gmail.com>
2022-02-08 09:45:39 -0300

    add assoc

--- src/app/composition.clj
+++ src/app/composition.clj
@@ -1,16 +1,26 @@
 (ns app.composition)
 
 (def transactions
-  [{:type "Deposit" :amount 500.00 :currency "R$"}
-   {:type "Withdraw" :amount 300.00 :currency "R$"}
-   {:type "Withdraw" :amount 50.00 :currency "R$"}
-   {:type "Deposit" :amount 1000.00 :currency "R$"}])
+  [{:type "Deposit" :amount 500.00 :date "2022-01-01" :currency "$"}
+   {:type "Withdraw" :amount 300.00 :date "2022-01-06"}
+   {:type "Withdraw" :amount 50.00 :date "2022-01-15" :currency "$"}
+   {:type "Deposit" :amount 1000.00 :date "2022-01-19" :currency "$"}])
 
-(defn transaction-symbol [transactions]
-  (let [amount (:amount transactions)
-        currency (:currency transactions)]
+(defn amount-symbol [transactions]
+  (let [currency (:currency transactions "$")
+        amount (:amount transactions)]
     (if (= (:type transactions) "Deposit")
       (str currency " +" amount)
       (str currency " -" amount))))
 
-(println (transaction-symbol (first transactions)))
+(defn date-value [transactions]
+  (str (:date transactions) " => "
+       (amount-symbol transactions)))
+
+(defn transactions-in-real [transactions]
+  (assoc transactions
+    :amount (* 5.50 (:amount transactions))
+    :currency "R$"))
+
+(println (date-value (first transactions)))
+(println (transactions-in-real (first transactions)))
