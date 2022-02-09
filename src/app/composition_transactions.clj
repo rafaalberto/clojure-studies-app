d8e51177fea4535c713f60dc2e401a72973b49b9
Rafael Alberto <rafael.alberto1703@gmail.com>
2022-02-08 21:36:12 -0300

    composition exercise

---
+++ src/app/composition_transactions.clj
@@ -0,0 +1,44 @@
+(ns app.composition-transactions)
+
+(def transactions
+  [{:type "Deposit" :amount 523.31M :date "2022-01-01" :currency "R$"}
+   {:type "Withdraw" :amount 300.00M :date "2022-01-06"}
+   {:type "Withdraw" :amount 50.00M :date "2022-01-15" :currency "R$"}
+   {:type "Deposit" :amount 1000.00M :date "2022-01-19" :currency "R$"}])
+
+(defn amount-report [transactions]
+  (let [currency (:currency transactions "$")
+        amount (:amount transactions)]
+    (if (= (:type transactions) "Deposit")
+      (str currency " +" amount)
+      (str currency " -" amount))))
+
+(defn transactions-report [transactions]
+  (str (:date transactions) " => "
+       (amount-report transactions)))
+
+(def currencies {:dollar {:price 5.44M :symbol "$"}})
+
+(defn transactions-in-dollar [transactions]
+  (let [dollar (:dollar currencies)]
+    (assoc transactions
+      :amount (* (:price dollar) (:amount transactions))
+      :currency (:symbol dollar))))
+
+;with composition
+(def report-in-dollar (comp transactions-report transactions-in-dollar))
+
+(println (map report-in-dollar transactions))
+
+;without composition - option 1
+;(defn transactions-report-in-dollar [transactions]
+;  (transactions-report (transactions-in-dollar transactions)))
+;
+;(println (map transactions-report-in-dollar transactions))
+
+;without composition - option 2
+;(defn transactions-report-in-dollar [transactions]
+;  (-> (transactions-in-dollar transactions)
+;      (transactions-report)))
+
+;(println (map transactions-report-in-dollar transactions))
