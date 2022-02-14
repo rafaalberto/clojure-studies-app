f99c048e9b3fb4b77ac05f51b7b63f7085bf06fa
Rafael Alberto <rafael.alberto1703@gmail.com>
2022-02-13 21:22:59 -0300

    refactoring

--- src/app/report_card.clj
+++ src/app/report_card.clj
@@ -1,9 +1,10 @@
 (ns app.report-card)
 
-(def grades-report {:grades [10 8 6 9]})
+(def grades-report {:grades [10 8 6]})
 
 (defn average [grades-report]
-  (let [grades (:grades grades-report) total (reduce + grades)]
+  (let [{grades :grades} grades-report
+        total (reduce + grades)]
     (double (/ total (count grades)))))
 
 (defn print-report [grades-report]
