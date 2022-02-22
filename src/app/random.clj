a1f6548fab71e0d03c69cd9aed1ea7d9043d84c0
Rafael Alberto <rafael.alberto1703@gmail.com>
2022-02-22 17:52:04 -0300

    created random transactions to test lazy sequence

--- src/app/random.clj
+++ src/app/random.clj
@@ -4,7 +4,7 @@
   {:value (* (rand-int 100001) 0.01M)
    :type  (rand-nth ["Deposit" "Withdraw"])})
 
-;repeatedly - starts lazy sequence
+;repeatedly - builds lazy sequence
 ;if number after repeatedly is not defined - it will be infinity
 (def transactions (repeatedly 3 random-transaction))
 
