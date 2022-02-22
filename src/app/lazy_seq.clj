734747f5482aadfdd86758ac402b5320bc254992
Rafael Alberto <rafael.alberto1703@gmail.com>
2022-02-22 20:27:34 -0300

    created lazy-seq example

--- src/app/lazy_seq.clj
+++ src/app/lazy_seq.clj
@@ -2,11 +2,17 @@
 
 (def numbers '(1 2 3 4))
 
-(def new-numbers (range 1 10001))
+(def new-numbers (range 1 900001))
 
-(defn add-items [numbers new-numbers]
+(defn add-items-recur [numbers new-numbers]
   (if-let [current-number (first new-numbers)]
     (recur (cons current-number numbers) (rest new-numbers))
     numbers))
 
-(prn (add-items new-numbers numbers))
+(defn add-items-lazy [numbers new-numbers]
+  (lazy-seq (if-let [current-number (first new-numbers)]
+    (add-items-lazy (cons current-number numbers) (rest new-numbers))
+    numbers)))
+
+(println "Time with Recur: " (time (class (add-items-recur numbers new-numbers))))
+(println "Time with Lazy-Seq: " (time (class (add-items-lazy numbers new-numbers))))
