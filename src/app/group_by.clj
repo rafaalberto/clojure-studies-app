5b13c8e89285d2267adc8915a98f6049ccf812c9
Rafael Alberto <rafael.alberto1703@gmail.com>
2022-10-04 20:09:22 -0300

    refactoring

--- src/app/group_by.clj
+++ src/app/group_by.clj
@@ -1,10 +1,14 @@
 (ns app.group-by)
 
 (defn print-grades []
-  (let [grades [3 10 4.5 5 10 9 8 0]
+  (let [grades '({:name "Rafael" :grade 10}
+                 {:name "Jim" :grade 4.5}
+                 {:name "Mary" :grade 9.5}
+                 {:name "John" :grade 3}
+                 {:name "Rose" :grade 8})
         {approved true
-         reproved false} (group-by #(>= % 5) grades)]
-    {:approved approved
-     :reproved reproved}))
+         reproved false} (group-by #(>= (:grade %) 5) grades)]
+    {:approved (seq approved)
+     :reproved (seq reproved)}))
 
 (print-grades)
